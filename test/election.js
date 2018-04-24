ffea42c457c56499e517e0a14fb3d1420cf33888
Saumitra Shukla <saumitra90@gmail.com>
2018-04-24 00:13:08 -0700

    Added test file for smart contract

---
+++ test/election.js
@@ -0,0 +1,13 @@
+var Election = artifacts.require("./Election.sol");
+
+contract("Election", function(accounts) {
+    var electionInstance;
+
+    it("initializes with two candidates", function() {
+        return Election.deployed().then(function(instance) {
+            return instance.candidatesCount();
+        }).then(function(count) {
+            assert.equal(count, 3);
+        });
+    });
+    });
