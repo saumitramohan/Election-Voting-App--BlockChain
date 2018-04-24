af1652c57322907a9e812558011e894e2d8f9fc0
Saumitra Shukla <saumitra90@gmail.com>
2018-04-23 23:51:35 -0700

    Added list of Candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,42 @@
+// Version of solidity
 pragma solidity ^0.4.11;
 
 contract Election {
+	// Model a Candidate
 	// Store Candidate
-	// Read Candidate
-	// Constructor
-	string public candidate;
+	// Fetch Candidate
+	// Store Candidate Count
+
+	// Make a structure of Candidate
+
+	// Model a Candidate
+
+	struct Candidate{
+		uint id;
+		String name;
+		uint voteCount;
+
+	}
 
 	function Election() public {
-		candidate = "Candidate 1";
+		addCandidate ("Candidate 1");
+		addCandidate ("Candidate 2");
+	}
+
+	// Store a Candidate
+
+	mapping (uint => Candidate) public candidates;
+
+	// Keep track as solidity doesnt provide any such method
+	uint public candidatesCount;
+
+	// Add a candidate
+
+	// Local variable - _name , hence _
+	// private as we dont want to be accesible
+	function addCandidate(String _name) private{
+		// Id of the candidates
+		candidatesCount ++;
+		candidates[candidatesCount] = Candidate(candidatesCount,_name,0);
 	}
 }
